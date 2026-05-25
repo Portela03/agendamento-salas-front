b9f977591720293a2fca370cab3fc83dede0260f
Gabriel Pardo <136826784+GabrieIPardo@users.noreply.github.com>
2026-05-25 18:50:15 -0300

    Correção para rodar corretamente no modo preview

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,6 +16,7 @@ const buttonVariants = cva(
         default: 'h-11 px-5',
         sm: 'h-9 px-4 text-xs',
         lg: 'h-12 px-6 text-base',
+        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
